9577106c69af2422cda3dc001eacaef5b8da55ff
joyo11 <shafay11august@gmail.com>
2025-01-25 15:19:46 -0500

    Initial Commit

---
+++ WeatherCast/WeatherCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCastApp.swift
+//  WeatherCast
+//
+//  Created by shafay11aug on 25/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
